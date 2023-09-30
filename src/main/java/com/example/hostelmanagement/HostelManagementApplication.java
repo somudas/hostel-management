612b20f21c101cd818f28f2051bd2a57a18152fb
somudas <soumyadeep4122002@gmail.com>
2023-09-30 15:51:06 +0530

    first commit

---
+++ src/main/java/com/example/hostelmanagement/HostelManagementApplication.java
@@ -0,0 +1,14 @@
+package com.example.hostelmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class HostelManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HostelManagementApplication.class, args);
+	}
+
+}
