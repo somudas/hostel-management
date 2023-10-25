800477205908e2a2fe816b72ad0971acf2564b38
Ujjawal Modi <umodi2003@gmail.com>
2023-10-25 21:43:28 +0530

    Added services feature to application.

---
+++ src/main/java/com/example/hostelmanagement/controller/ServiceController.java
@@ -0,0 +1,63 @@
+package com.example.hostelmanagement.controller;
+
+import com.example.hostelmanagement.model.*;
+import com.example.hostelmanagement.service.ComplaintService;
+import com.example.hostelmanagement.service.MemberService;
+import com.example.hostelmanagement.service.ServiceService;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+import java.util.List;
+
+@Controller
+public class ServiceController {
+
+    private final ServiceService serviceService;
+    private final MemberService memberService;
+
+    @Autowired
+    public ServiceController(ServiceService serviceService, MemberService memberService){
+        this.serviceService=serviceService;
+        this.memberService=memberService;
+    }
+
+    @GetMapping("/services")
+    private String getServices(Principal principal, Model model){
+        User user = memberService.findUser(principal.getName());
+
+        if(user.getRole().equals("WARDEN")){
+            model.addAttribute("services", serviceService.getAllServices());
+        }
+        else {
+            model.addAttribute("services", serviceService.getAllServicesAssignedToMember(user.getMid(), user.getRole()));
+        }
+        model.addAttribute("isAdmin", user.getRole().equals("WARDEN"));
+        return "services";
+    }
+
+    @PostMapping("/services/add")
+    private String addService(Principal principal, Model model,@ModelAttribute Service service){
+        service.setAssignedToRole("STAFF");
+        System.out.println(serviceService.addService(service));
+        return "redirect:/services";
+    }
+
+    @GetMapping("/update/service/{id}")
+    @ResponseBody
+    private Integer updateService(@PathVariable("id") Integer serviceId){
+        Service service= new Service();
+        service.setServiceId(serviceId);
+        return serviceService.updateServiceStatus(service);
+    }
+
+    @GetMapping("/services/add")
+    public String addService(Principal principal, Model model) {
+        model.addAttribute("service", new Service());
+        model.addAttribute("allMembers",memberService.viewAll());
+        return "registerService";
+    }
+}
---
+++ src/main/java/com/example/hostelmanagement/dao/ServiceDao.java
@@ -0,0 +1,55 @@
+package com.example.hostelmanagement.dao;
+
+import com.example.hostelmanagement.model.Message;
+import com.example.hostelmanagement.model.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.util.List;
+
+@Repository
+public class ServiceDao {
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public ServiceDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public Integer addService (Service service){
+
+        final String sql = "INSERT INTO services(serviceName, assignedToId, assignedToRole) values(?,?,?)";
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection
+                    .prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, service.getServiceName());
+            ps.setInt(2, service.getAssignedToId());
+            ps.setString(3, service.getAssignedToRole());
+            return ps;
+        }, keyHolder);
+        return keyHolder.getKey().intValue();
+    }
+
+    public List<Service> getAllServices(){
+        final String sql="Select * from services";
+        return jdbcTemplate.query(sql,new BeanPropertyRowMapper<>(Service.class));
+    }
+
+    public List<Service> getAllServicesAssignedToMember(Integer memberId, String memberRole){
+        final String sql=String.format("Select * from services where assignedToId=%d and assignedToRole='%s'",memberId,memberRole);
+        return jdbcTemplate.query(sql,new BeanPropertyRowMapper<>(Service.class));
+    }
+
+    public Integer updateServiceStatus (Integer serviceId){
+        final String sql=String.format("UPDATE services SET lastUpdatedOn=NOW() where serviceId=%d",serviceId);
+        return jdbcTemplate.update(sql);
+    }
+}
